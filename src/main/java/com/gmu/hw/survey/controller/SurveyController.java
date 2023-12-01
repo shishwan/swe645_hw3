f18a218f66493e5a73e745e324cfd500d7b2a6c9
shishwan <113803541+shishwan@users.noreply.github.com>
2023-11-30 22:42:14 -0500

    Update SurveyController.java

--- src/main/java/com/gmu/hw/survey/controller/SurveyController.java
+++ src/main/java/com/gmu/hw/survey/controller/SurveyController.java
@@ -21,7 +21,7 @@ public class SurveyController {
 
     @GetMapping("/home")
     public String home(){
-        return  "Campus Survey APP running on Jenkins";
+        return  "Campus Survey APP running on Rancher after Jenkins Build";
     }
 
 
